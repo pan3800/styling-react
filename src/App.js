6f900321e3ec920bd36ffc8d28da97d2c58c5fa0
Chae Seong Lim <pan1129@naver.com>
2023-10-20 00:35:54 +0900

    sass 사용법

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import { Component } from "react";
-import logo from './logo.svg';
-import './App.css';
+import SassComponent from "./SassComponent";
 
 class App extends Component {
   render() {
-    return(
-      <div className="App">
-        <header>
-          <img src="{logo}" className="logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> add save to reload
-          </p>
-          <a 
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Re`act
-          </a>
-        </header>
+    return (
+      <div>
+        <SassComponent />
       </div>
-    )
+    );
   }
 }
 
